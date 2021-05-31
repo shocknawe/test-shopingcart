ddb7a78b1b4d8d4c9725404dcc60ae30462c135f
Jairus Ser Aragon <jairus.aragon@zuhlke.com>
2021-05-31 13:20:24 +0800

    create and passed test for conditions

--- transaction/logic.go
+++ transaction/logic.go
@@ -1,15 +1,55 @@
 package transaction
 
+import (
+	"log"
+	"math"
+	"regexp"
+	"strconv"
+)
+
 func ComputeTotal(transaction *Transaction) int64 {
 	return 0
 }
 
 func ComputeLineTotal(line_item *TransactionLineItem) int64 {
+	var total int64
 	product := line_item.GetProduct()
-	productPrice := product.GetPrice()
 	quantity := line_item.GetQuantity()
+	discount := line_item.GetDiscount()
+	condition := discount.GetDiscountCondition()
+	lineTotal := product.GetPrice() * int64(quantity)
 
-	total := productPrice * int64(quantity)
+	didIPassCondition := false
+
+	if condition == "" {
+		// no condition for discount
+		didIPassCondition = true
+	} else {
+		qCondition := regexp.MustCompile(`^q>=(\d+)/?`)
+		matches := qCondition.FindStringSubmatch(condition)
+		if len(matches) == 2 {
+			qConditionNum, err := strconv.Atoi(matches[1])
+			if err != nil {
+				log.Printf("Couldn't convert to in %v\n", matches[1])
+			}
+			if quantity >= int32(qConditionNum) {
+				didIPassCondition = true
+			}
+		}
+	}
+
+	switch discount.GetDiscountType() {
+	case Discount_DISCOUNTTYPE_NONE:
+		total = lineTotal
+	case Discount_DISCOUNTTYPE_PERCENT:
+		if !didIPassCondition {
+			total = lineTotal
+			break
+		}
+		discountedValue := float32(lineTotal) * (1.0 - discount.GetDiscountValue())
+		discountedValue = float32(math.Round(float64(discountedValue)))
+		total = int64(discountedValue)
+	}
 
 	return total
 }
--- transaction/logic_test.go
+++ transaction/logic_test.go
@@ -17,8 +17,8 @@ func TesComputeTotal(t *testing.T) {
 	})
 }
 
-// TestComputeLineTotal does a single entry test with no discount
-func TestComputeLineTotal(t *testing.T) {
+// TestComputeLineTotalNoDiscount does 1 product with no discount
+func TestComputeLineTotalNoDiscount(t *testing.T) {
 	expected := int64(49990)
 	actual := ComputeLineTotal(&TransactionLineItem{
 		Product: &Product{
@@ -27,6 +27,46 @@ func TestComputeLineTotal(t *testing.T) {
 		Quantity: 1,
 		Discount: &Discount{
 			DiscountType: Discount_DISCOUNTTYPE_NONE,
+		},
+	})
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("Should return %v instead of %v", expected, actual)
+	}
+}
+
+// TestComputeLineTotalDiscountPercent 1 product with percent discount
+func TestComputeLineTotalDiscountPercent(t *testing.T) {
+	expected := int64(44991)
+	actual := ComputeLineTotal(&TransactionLineItem{
+		Product: &Product{
+			Price: 49990,
+		},
+		Quantity: 1,
+		Discount: &Discount{
+			DiscountType:  Discount_DISCOUNTTYPE_PERCENT,
+			DiscountValue: .10,
+		},
+	})
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("Should return %v instead of %v", expected, actual)
+	}
+}
+
+// TestComputeLineTotalDiscountPercentWithCondition 1 product with percent discount with condition
+func TestComputeLineTotalDiscountPercentWithCondition(t *testing.T) {
+	// 2 for the price of 1
+	expected := int64(219000)
+	actual := ComputeLineTotal(&TransactionLineItem{
+		Product: &Product{
+			Sku:   "A304SD",
+			Name:  "AlexaSpeaker",
+			Price: 109500,
+		},
+		Quantity: 3,
+		Discount: &Discount{
+			DiscountType:      Discount_DISCOUNTTYPE_PERCENT,
+			DiscountValue:     1.0 / 3.0,
+			DiscountCondition: "q>=3",
 		},
 	})
 	if !reflect.DeepEqual(actual, expected) {
